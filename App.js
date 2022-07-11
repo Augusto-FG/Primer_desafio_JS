22778e5113380282fc4760482a01a93cf486edd8
Augusto Fernández <79116699+Augusto-FG@users.noreply.github.com>
2022-07-11 20:23:10 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-alert ("bienvenido a la calculadora (solo se restar y sumar");
-let a = parseInt(prompt("Ingrese primer numero"));
-let b = parseInt(prompt("Ingrese segundo numero"));
+alert ("bienvenido a la calculadora");
+let a = parseInt(prompt("ingrese primer numero"));
+let b = parseInt(prompt("ingrese segundo numero"));
 
 function sumar (a,b) {
     c = a+b
@@ -12,10 +12,41 @@ function restar (a,b) {
         return alert (c);
         }
 
-let operacion = parseInt(prompt("ingrese la operacion deseada (sumar o restar)"));
+function multiplicar (a,b) {
+        c = a*b
+        return alert (c);
+        }
 
-if (operacion = "sumar"){
+function dividir (a,b) {
+        c = a/b
+        return alert (c);
+        }
+
+let operacion = parseInt(prompt("ingrese 1 para sumar, 2 para restar, 3 para multiplizar, 4 para dividir)"));
+
+/* if (operacion = "sumar"){
     sumar(a,b);
 } else {
     restar(a,b);
-} 
+}  */
+
+switch (operacion) {
+    case 1: sumar(a,b);
+        
+        break;
+    case 2: restar(a,b);
+        
+        break;
+    
+    case 3: multiplicar(a,b);
+        
+        break;
+    
+    case 4: dividir(a,b);
+        
+        break;
+
+    default:
+        alert("Ha elegido una opción incorrecta");
+        break;
+}
